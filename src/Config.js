bb6a5f50b09288aa3dc9641760676dd1fe348b4a
Jim Renwick <jim.renwick@webgp.com>
2020-02-18 13:04:48 +0000

    added rendering for box types

--- src/Config.js
+++ src/Config.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {useSelector} from "react-redux";
 import InputGroup from "react-bootstrap/InputGroup";
-import {iconDragYDistance, levelsInViewPortCount} from "./config/constants";
+import {BoxTypeClasses, iconDragYDistance, levelsInViewPortCount} from "./config/constants";
 import {InputField} from "./InputField";
 import {boxConfigUpdated} from "./redux/actions";
 import {store} from "./redux/store";
+import Badge from "react-bootstrap/Badge";
 
 
 export function Config(props) {
@@ -26,6 +27,7 @@ export function Config(props) {
         store.dispatch(boxConfigUpdated(focusedId, text, e.target.value));
     }
 
+    // TODO separate rendering out into different components
     if(context === "VIEW_PORT" && focusedId) {
 
         let config = getConfig(focusedId);
@@ -39,26 +41,40 @@ export function Config(props) {
                                 <label className="capitalise" htmlFor={item[0]}>{item[0]}</label>
                                 {
                                     Array.isArray(item[1]) ?
-                                    item[1].map((it, index) => (
-                                        <InputGroup key={item[0] + index} className="mb-3">
-                                            <InputField id={index} for={item[0] + ":" + index} defaultValue={it} callback={fieldChangeCallback}/>
+                                        item[1].map((it, index) => (
+                                            <InputGroup key={item[0] + index} className="mb-3">
+                                                <InputField id={index} for={item[0] + ":" + index} defaultValue={it}
+                                                            callback={fieldChangeCallback}/>
+                                            </InputGroup>
+                                        )) :
+                                        <InputGroup key={item[0]} className="mb-3">
+                                            <InputField id={item[0]} for={item[0]} defaultValue={item[1]}
+                                                        callback={fieldChangeCallback}/>
                                         </InputGroup>
-                                    )) :
-                                    <InputGroup key={item[0]} className="mb-3">
-                                        <InputField id={item[0]} for={item[0]} defaultValue={item[1]} callback={fieldChangeCallback}/>
-                                    </InputGroup>
                                 }
                             </div>
-                        )})
+                        )
+                    })
                 }
             </div>
         )
-    } else if (context === "BOX_TYPE_BAR") {
+    } else if(context === "BOX_TYPE_BAR") {
         return (
-            <div className="config">{focusBoxType}</div>
+            <div className="config">
+                <h4 className="italicise">{focusBoxType}</h4>
+                <br/>
+                {
+                    Object.entries(BoxTypeClasses[focusBoxType]).map(item => {
+                        return (
+                            <h6 className="capitalise">{item[0]}
+                                <Badge style={{"float": "right"}} variant="secondary">{item[1]["input"]}</Badge>
+                            </h6>
+                        )
+                    })
+                }
+            </div>
         )
-    }
-    else {
+    } else {
         return (
             <div className="config">NOTHING</div>
         )
