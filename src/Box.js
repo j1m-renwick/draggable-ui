92e1fc43fe46956ef285b0c5e6e4ac4f417e9800
Jim Renwick <jim.renwick@webgp.com>
2020-02-19 13:19:01 +0000

    added state to check overlaps and cancel dragging

--- src/Box.js
+++ src/Box.js
@@ -1,12 +1,20 @@
 import Draggable from "react-draggable";
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {boxDragged, boxInViewPortFocused, boxLocationSet} from "./redux/actions";
 import {store} from "./redux/store"
 import {iconDragXDistance, iconDragYDistance, iconDiameter, iconSpacingYMargin} from "./config/constants";
+import {useSelector} from "react-redux";
 
 export function Box(props) {
 
-    useEffect(() => {store.dispatch(boxLocationSet(props.boxId, props.x, props.y))});
+    const [x, setX] = useState(props.x);
+    const [y, setY] = useState(props.y);
+
+    useEffect(() => {
+        store.dispatch(boxLocationSet(props.boxId, x, y))
+    });
+
+    const boxLocations = useSelector(state => state.locations);
 
     const additionalStyling = {
         "width": iconDiameter + "px",
@@ -25,9 +33,25 @@ export function Box(props) {
         store.dispatch(boxDragged(props.boxId, position.x, position.y));
     }
 
+    // check that the stopped position doesn't overlap with any other boxes - if it does, reset it back to the original position before the drag started.
+    function onStopDrag(e, position) {
+        let overlappingItem = boxLocations.filter(item => item.id !== props.boxId).find(item => item.x === position.lastX && item.y === position.lastY);
+        if(overlappingItem !== undefined) {
+            setX(null);
+            setX(x);
+            store.dispatch(boxDragged(props.boxId, x, y));
+        } else {
+            setX(position.lastX);
+            setY(position.lastY);
+        }
+    }
+
+
     return (
-        <Draggable grid={[iconDragXDistance, iconDragYDistance]} defaultPosition={{x: props.x ? props.x : 0, y: props.y ? props.y : 0}} bounds=".grid" onDrag={onControlledDrag}>
-            <div className={props.type} style={additionalStyling} onClick={() => select()} />
+        <Draggable grid={[iconDragXDistance, iconDragYDistance]}
+                   defaultPosition={{x: props.x ? props.x : 0, y: props.y ? props.y : 0}} position={{x: x, y: y}}
+                   bounds=".grid" onDrag={onControlledDrag} onStop={onStopDrag}>
+            <div className={props.type} style={additionalStyling} onClick={() => select()}/>
         </Draggable>
     )
 
