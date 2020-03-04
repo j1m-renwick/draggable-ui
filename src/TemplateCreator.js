43f83c6d9febbbc93349ee1556698624a228ca27
Jim Renwick <jim.renwick@webgp.com>
2020-03-04 13:01:18 +0000

    remove example state

--- src/TemplateCreator.js
+++ src/TemplateCreator.js
@@ -19,91 +19,6 @@ const codeGenerator = (prefix, typeCode) => {
     return instance;
 };
 
-const exampleToSave = {
-    currentLevel: 0,
-    linkageInProgress: false,
-    linkageReference: null,
-    hoveredLinkId: null,
-    boxes: {
-        '9890c7a3-033f-4d13-9176-27b72915682a': {
-            id: '9890c7a3-033f-4d13-9176-27b72915682a',
-            type: 'spiral-icon',
-            x: 0,
-            y: 0
-        },
-        '5c6e4997-ab58-442a-be0b-ca91eb18d63b': {
-            id: '5c6e4997-ab58-442a-be0b-ca91eb18d63b',
-            type: 'equals-icon',
-            x: 0,
-            y: 121
-        },
-        '01072689-9163-4d3f-987f-7483bb7bfc37': {
-            id: '01072689-9163-4d3f-987f-7483bb7bfc37',
-            type: 'pic-icon',
-            x: 100,
-            y: 121
-        }
-    },
-    config: {
-        '9890c7a3-033f-4d13-9176-27b72915682a': {
-            title: {
-                value: 'item one'
-            },
-            question: {
-                value: 'Who\'s house?'
-            },
-            answers: {
-                '061f17f8-ef1d-4e6e-845e-9c06a432ca20': {
-                    value: 'Run\'s house',
-                    linkable: true,
-                    linkedId: '5c6e4997-ab58-442a-be0b-ca91eb18d63b'
-                },
-                '03dc2743-a9c0-4c80-90f3-901c5cea0765': {
-                    value: 'Joe\'s house',
-                    linkable: true,
-                    linkedId: '01072689-9163-4d3f-987f-7483bb7bfc37'
-                }
-            }
-        },
-        '5c6e4997-ab58-442a-be0b-ca91eb18d63b': {
-            title: {
-                value: 'item two'
-            },
-            question: {
-                value: 'Enough now?'
-            },
-            answers: {
-                'e8a41ae0-fd8e-4bd4-aec8-65dcc4da64f9': {
-                    linkable: true,
-                    value: 'Yes'
-                }
-            }
-        },
-        '01072689-9163-4d3f-987f-7483bb7bfc37': {
-            title: {
-                value: 'item three'
-            },
-            question: {
-                value: 'Hello?'
-            },
-            answers: {
-                '8119942f-c12c-424c-94fb-61e243e8e773': {
-                    linkable: true,
-                    value: 'Yes'
-                }
-            }
-        }
-    },
-    children: {
-        '9890c7a3-033f-4d13-9176-27b72915682a': [
-            '5c6e4997-ab58-442a-be0b-ca91eb18d63b',
-            '01072689-9163-4d3f-987f-7483bb7bfc37'
-        ],
-        '5c6e4997-ab58-442a-be0b-ca91eb18d63b': [],
-        '01072689-9163-4d3f-987f-7483bb7bfc37': []
-    }
-};
-
 export function process(stateJson) {
 
     let generatedJson = {};
@@ -183,5 +98,3 @@ export function process(stateJson) {
         return question;
     }
 }
-
-// console.dir(process(exampleToSave));
