348c8decb6d198262aa55aa34d1ee6b68e922ef2
Jim Renwick <jim.renwick@webgp.com>
2020-03-06 09:47:27 +0000

    updated save link to properly transform file

--- src/SaveNavLink.js
+++ src/SaveNavLink.js
@@ -2,19 +2,36 @@ import {Nav} from "react-bootstrap";
 import React from "react";
 import saveAs from 'file-saver';
 import {store} from "./redux/store";
+import {iconDragYDistance} from "./config/constants";
 
 export function SaveNavLink() {
 
     function saveFile() {
-        // TODO properly transform state and have a pop up with filename to save as
-        let blobToSave = new Blob([store.getState()], {type: "text/plain;charset=utf-8"});
+        // TODO have a pop up with filename to save as
+        let blobToSave = new Blob([JSON.stringify(transformToSaveFile(store.getState()))], {type: "text/plain;charset=utf-8"});
         let dateTime = new Date();
         let date = dateTime.toLocaleDateString().replace(/\/|\\/g, "-");
         let time = dateTime.toLocaleTimeString().replace(/:/g, ".");
         saveAs(blobToSave, "UI FLOW " + date + " at " + time + ".json");
     }
 
-    return <Nav.Link onClick={saveFile}>Save</Nav.Link>
+    function transformToSaveFile(state) {
+        // TODO add project details in
+        // add child
+        return Object.entries(state.boxes).map(entry => {
+            let box = {};
+            let entryKey = entry[0];
+            let entryValue = entry[1];
+            box.id = entryKey;
+            box.type = entryValue.type;
+            box.level = entryValue.y / iconDragYDistance;
+            box.config = state.config[entryKey];
+            box.children = state.children[entryKey];
+            return box;
+        })
 
+    }
+
+    return <Nav.Link onClick={saveFile}>Save</Nav.Link>
 
 }
